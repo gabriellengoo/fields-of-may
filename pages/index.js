cc00a28abf7679fb1969cac764d08dedcade286f
gabriellengoo <gabriellengoo@hotmail.com>
2022-08-20 17:59:49 +0100

    init

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,12 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import * as React from 'react';
+
 
 export default function Home() {
+
+
     return (
         <div className={
             styles.container
@@ -11,41 +15,284 @@ export default function Home() {
                 <title>Fields of May</title>
                 <meta name="description" content="Fields of May"/>
                 <link rel="icon" href="/favicon.ico"/>
+                <script type="text/javascript" src="//code.jquery.com/jquery-1.10.2.min.js" defer></script>
+                <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js" defer></script>
+                <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js" defer></script>
+                <script src="assets/app.js" defer></script>
             </Head>
+
 
             <main className={
                 styles.main
             }>
-                {/* <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p> */}
 
-{/* <section className={styles.gradientTop}></section> */}
+                {/* <Image id="image" className={styles.cursorimg} src={cursor}  alt="Herring Fish" width="400px" height="350px"/> */}
 
-                <section>
-                    <h1 className={
-                        styles.livetext
+
+                {/*---------------------------- header----------- */}
+
+
+                <header className={
+                    styles.header
+                }>
+                    <div className={
+                        styles.wrapperHeader
                     }>
-                        Fields of May
-                    </h1>
+
+                        <div className={
+                            styles.containerHeader
+                        }>
+
+
+                            <div className={
+                                styles.headertime
+                            }>
+                                <div lassName={styles}>
+                                    <ul className={
+                                        styles.ul
+                                    }>
+                                        {/* <h4 className={
+                                                styles.spaceingAxies
+                                            }id="log">pageX: -, pageY: - </h4> */}
+                                        
+                                        <span className={
+                                                styles.fieldtext
+                                            }
+                                            id="day">day</span>
+                                        <span className={
+                                                styles.fieldtext
+                                            }
+                                            id="daynum">00</span>
+                                        <span className={
+                                                styles.fieldtext
+                                            }
+                                            id="month">month</span>
+
+
+                                        <li className={
+                                                styles.spaceingTime
+                                            }
+                                            id="hours"></li>
+                                        <li id="point">:</li>
+                                        <li id="min"></li>
+                                        <li id="point">:</li>
+                                        <li id="sec"></li>
+                                    </ul>
+                                </div>
+
+                                <div></div>
+                            </div>
+
+
+                        </div>
+                    </div>
+                </header>
+
+
+                {/*---------------------------- horizontal gradient ----------- */}
+                <section className={
+                    styles.gradientTop
+                }></section>
+
+
+                {/*---------------------------- centered text ----------- */}
+                <section className={
+                    styles.centeredElements
+                }>
+
+                    <div className={
+                        styles.subMenu
+                    }>
+                        <a className={
+                                styles.linkTop
+                            }
+                            href="./">Witness Seminars []
+                        </a>
+                        <a className={
+                                styles.linkTop
+                            }
+                            href="./">Past Witness Seminars []</a>
+                        <a className={
+                                styles.linkTop
+                            }
+                            href="./about">about []</a>
+                    </div>
+
+
+                    {/*---------------------------- live text ----------- */}
+
+
+                    <div className={
+                        styles.livetextWrapper
+                    }>
+                        <div className={
+                            styles.livetextContainer
+                        }>
+
+                            {/* <h4 id="log">pageX: -, pageY: -</h4> */}
+
+                            <div id="tooltip"
+                                className={
+                                    styles.livetext
+                            }>
+                                Fields of May
+                            </div>
+
+
+                        </div>
+                    </div>
+
+
+                    {/* <Image id="image" className={styles.cursorimg} src={cursor}  alt="Herring Fish" width="400px" height="350px"/> */}
+
+                    {/* <div class="centerdiv"livetextWrapper>
+<div class="container">
+        <div id="follower"></div>
+</div></div>*/}
+
+
+                    {/*---------------------------- Sub text ----------- */}
+                    <div className={
+                        styles.subText
+                    }>
+                        <p className={
+                            styles.subTextOpacity
+                        }>Opacity representing the Amount of
+                            <a href="https://www.google.com/maps/place/Baltic+Sea/@57.2610777,19.7297928,6z/data=!3m1!4b1!4m5!3m4!1s0x46f4d7d988201b2b:0xb43097ae8474cb3!8m2!3d58.487952!4d19.863281"
+                                className={
+                                    styles.link
+                            }>Baltic Sea Algae</a>
+                            as of //DATEFETCHED//</p>
+
+
+                        <div className={
+                            styles.algaeListContainer
+                        }>
+
+
+                            <p className={
+                                styles.algaeListTitle
+                            }>Algae Concentration</p>
+
+                            <div className={
+                                styles.algaeListWrapper
+                            }>
+
+                                <div className={
+                                    styles.algaeList
+                                }>
+                                    <li>Excellent</li>
+                                    <li className={
+                                        styles.goodVision
+                                    }>Good</li>
+                                    <li className={
+                                        styles.fairVision
+                                    }>Fair</li>
+                                    <li className={
+                                        styles.poorVision
+                                    }>Poor</li>
+                                </div>
+                            </div>
+
+                            <div className={
+                                styles.algaeListWrapper
+                            }>
+                                <div className={
+                                    styles.algaeList
+                                }>
+                                    <li>0–2.0</li>
+                                    <li className={
+                                        styles.goodVision
+                                    }>2.1–2.5</li>
+                                    <li className={
+                                        styles.fairVision
+                                    }>2.6–5.8</li>
+                                    <li className={
+                                        styles.poorVision
+                                    }>5.9+</li>
+                                </div>
+                            </div>
+
+
+                            <div className={
+                                styles.algaeListWrapper
+                            }>
+                                <div className={
+                                    styles.algaeList
+                                }>
+                                    <li>UG/L</li>
+                                    <li className={
+                                        styles.goodVision
+                                    }>UG/L</li>
+                                    <li className={
+                                        styles.fairVision
+                                    }>UG/L</li>
+                                    <li className={
+                                        styles.poorVision
+                                    }>UG/L</li>
+                                </div>
+                            </div>
+                        </div>
+
+
+                    </div>
+                </section>
+
+
+                {/* Decorative-------------------------------------------------------- */}
+                <section className={
+                    styles.cordiantesContainer
+                }>
+                    <div className={
+                            styles.cordBottomLeft
+                        }
+                        id="cordBottomLeft">
+                        <p className={
+                            styles.cord
+                        }>+ (10°e)</p>
+                    </div>
+
+                    <div className={
+                            styles.cord__bottomMiddle
+                        }
+                        id="cordBottomMiddle">
+                        {/* <p className={styles.plus}>+</p> */}
+                        <p className={
+                            styles.cord
+                        }>+ (20°e)</p>
+                    </div>
+
+                    <div className={
+                            styles.cord__bottomEnd
+                        }
+                        id="cordBottomEnd">
+                        <p className={
+                            styles.cord
+                        }>+ (30°e)</p>
+                    </div>
                 </section>
 
 
             </main>
 
 
-            <footer className={
+            {/*---------------------------- footer----------- */}
+            {/* <footer className={
                 styles.footer
             }>
-                <a>
-                    footer
-                </a>
-            </footer>
-        </div>
+                <p>
+                Fields of May (2022), Public sculpture
+Artists: FRAUD (Audrey Samson & Francisco Gallardo)
+Rope weaving: Maija Toukolehto
+Woodwork: Joel Simberg
+Herbarium: Lotta Petronella
+Flag design: Francisca Roseiro
+Commissioner: Contemporary Art Archipelago (CAA)
+Collaboration with the Archipelago Research Institute.
+<a>http://fieldsofmay.net/</a>
+
+                </p>
+            </footer> */} </div>
     )
 }
---
+++ public/assets/app.js
@@ -0,0 +1,114 @@
+console.log("hi im runnng");
+
+
+
+
+
+
+
+  // ----------------------------------- follow ----------------------------------------------------
+
+//   $(document).mousemove(function(e){
+//     $("#image").css({left:e.pageX, top:e.pageY});
+// });
+$( document ).on( "mousemove", function( event ) {
+  $( "#log" ).text( "pageX: " + event.pageX - ", pageY: " + event.pageY );
+  $( "#tooltip" ).css({ "left" : event.pageX - 500, "top" : event.pageY - 500});
+  $( "#image" ).css({ "left" : event.pageX - 500, "top" : event.pageY - 500});
+});
+
+
+
+
+
+
+  // --------------------------------------------------------- time and date at top
+
+
+
+
+  $(document).ready(function() {
+    // Create two variables with names of months and days of the week in the array
+    var monthNames = [ "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" ]; 
+    var dayNames= ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
+    
+    
+  
+    
+    setInterval( function() {
+        // Create an object newDate () and extract the second of the current time
+        var seconds = new Date().getSeconds();
+        // Add a leading zero to the value of seconds
+        $("#sec").html(( seconds < 10 ? "0" : "" ) + seconds);
+        },1000);
+        
+    setInterval( function() {
+        // Create an object newDate () and extract the minutes of the current time
+        var minutes = new Date().getMinutes();
+        // Add a leading zero to the minutes
+        $("#min").html(( minutes < 10 ? "0" : "" ) + minutes);
+        },1000);
+        
+    setInterval( function() {
+        // Create an object newDate () and extract the clock from the current time
+        var hours = new Date().getHours();
+        // Add a leading zero to the value of hours
+        $("#hours").html(( hours < 10 ? "0" : "" ) + hours);
+        }, 1000);
+        
+    }); 
+  
+  
+  
+  
+  // Date
+  function updateDate() {
+    let today = new Date();
+  
+    // return number
+    let dayName = today.getDay(),
+      dayNum = today.getDate(),
+      month = today.getMonth();
+      // year = today.getFullYear();
+  
+    const months = [
+      "Jan",
+      "Feb",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "Aug",
+      "Sep",
+      "Oct",
+      "Nov",
+      "Dec",
+    ];
+    const dayWeek = [
+      "Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"
+    ];
+    // value -> ID of the html element
+    const IDCollection = ["day", "daynum", "month"];
+    const IDCollection2 = ["day2", "daynum2"];
+    // return value array with number as a index
+    const val = [dayWeek[dayName], dayNum, months[month], ];
+    for (let i = 0; i < IDCollection.length; i++) {
+      document.getElementById(IDCollection[i]).firstChild.nodeValue = val[i];
+    }
+  
+    const val2 = [dayWeek[dayName], dayNum, months[month], ];
+    for (let i = 0; i < IDCollection2.length; i++) {
+      document.getElementById(IDCollection2[i]).firstChild.nodeValue = val[i];
+    }
+  }
+  
+  updateDate();
+  
+
+
+
+
+
+ 
+  
