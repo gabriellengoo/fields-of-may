6bfb2d66441b4d74e0a85006cd628220056dd056
gabriellengoo <gabriellengoo@hotmail.com>
2022-08-24 02:10:54 +0100

    color fix

--- public/assets/app.js
+++ public/assets/app.js
@@ -11,11 +11,11 @@ console.log("hi im runnng");
 //   $(document).mousemove(function(e){
 //     $("#image").css({left:e.pageX, top:e.pageY});
 // });
-$( document ).on( "mousemove", function( event ) {
-  $( "#log" ).text( "pageX: " + event.pageX - ", pageY: " + event.pageY );
-  $( "#tooltip" ).css({ "left" : event.pageX - 500, "top" : event.pageY - 500});
-  $( "#image" ).css({ "left" : event.pageX - 500, "top" : event.pageY - 500});
-});
+// $( document ).on( "mousemove", function( event ) {
+//   $( "#log" ).text( "pageX: " + event.pageX - ", pageY: " + event.pageY );
+//   $( "#tooltip" ).css({ "left" : event.pageX - 500, "top" : event.pageY - 500});
+//   $( "#image" ).css({ "left" : event.pageX - 500, "top" : event.pageY - 500});
+// });
 
 
 
